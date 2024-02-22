47b8aa9848191f4c6f11c31acba65ed5c24abc18
François-Xavier Savary <fx.savary@orange.fr>
2024-02-22 14:05:44 +0100

    typage AdminStore

---
+++ src/stores/AdminStore.ts
@@ -0,0 +1,71 @@
+import { defineStore } from 'pinia';
+import { reactive } from 'vue';
+import * as api from '@/services/api'; // importe les fonctions de l'api du fichier api.js
+import type { Admin } from '@/types/adminsTypes';
+
+export const useAdminStore = defineStore('admins', {
+
+    state: () => ({
+        token: null as string | null, // initialise le token à null par défaut
+        isConnected: <boolean>false, // statut initial de l'administrateur à 'non-connecté'
+        adminData: reactive<Record<string, unknown>>({}), // déclare adminData comme réactif
+        admins: [] as Admin[], // initialise admins comme un tableau vide
+    }),
+
+    actions: {
+
+        // stocke le token obtenu à la connexion de l'administrateur
+        setToken(newToken: string | null) {
+            this.token = newToken;
+        },
+
+        // supprime le token à la déconnexion de l'administrateur
+        clearToken() {
+            this.token = null;
+        },
+
+        // sauvegarde les données de l'administrateur connecté dans le localStorage
+        saveAdminDataInLocalStorage(): void {
+            localStorage.setItem('adminData', JSON.stringify(this.adminData));
+        },
+
+        // charge les données de l'administrateur à partir de la réponse JSON
+        setAdminData(adminData: Admin): void {
+            this.adminData = reactive(adminData);
+            this.saveAdminDataInLocalStorage();
+        },
+
+        // charge les données de l'administrateur à partir du localStorage
+        async loadAdminDataFromLocalStorage() {
+            const localStorageAdminData = localStorage.getItem('adminData');
+            if (localStorageAdminData) {
+                this.adminData = JSON.parse(localStorageAdminData);
+            }
+        },
+
+        // récupère les données de tous les administrateurs à partir de api.js
+        async loadAdminsData() {
+            try {
+                const adminsData = await api.fetchAdminsData();
+                this.admins = adminsData;
+
+            } catch (error) {
+                console.error('Erreur lors du chargement des données des administrateurs');
+            }
+        },
+
+        // ajoute administrateur nouvellement créé à admins
+        addToAdmins(admin: Admin) {
+            this.admins.push(admin);
+        },
+    },
+
+    getters: {
+
+        // méthode pour filtrer les administrateurs en comparant avec l'id de l'administrateur actuellement connecté (admin connecté ne peut pas supprimer son propre compte)
+        filteredAdmins(): Admin[] {
+            const connectedAdminId = this.adminData.id;
+            return this.admins.filter(admin => admin.id !== connectedAdminId);
+        }
+    }
+});
