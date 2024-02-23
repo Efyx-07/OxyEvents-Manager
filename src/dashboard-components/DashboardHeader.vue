4fe1fc8116c62eb7fe77da466dfc5417c5ef8d38
François-Xavier Savary <fx.savary@orange.fr>
2024-02-23 15:34:14 +0100

    Structure design DB pages

---
+++ src/dashboard-components/DashboardHeader.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+interface DashboardHeaderProps {
+    title: string;
+}
+
+const props = defineProps<DashboardHeaderProps>();
+
+</script>
+
+<template>
+    <div class="dashboardHeader">
+      <h1>{{ props.title }}</h1>
+      <div class="content">
+        <slot></slot>
+      </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.dashboardHeader {
+    width: 100%;
+    height: 6rem;
+    background: red;
+    display: flex;
+    justify-content: space-between;
+
+    h1 {
+        margin: 0;
+    }
+}
+</style>
--- src/views/dashboard-views/HomePage.vue
+++ src/views/dashboard-views/HomePage.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 
 import NavAside from '@/dashboard-components/NavAside.vue';
+import DashboardHeader from '@/dashboard-components/DashboardHeader.vue';
 
 </script>
 
 <template>
-    <div class="backOfficeHomePage">
+    <div class="dashboardPage">
         <NavAside />
+        <div class="page-content">
+            <DashboardHeader title="JE SUIS LE TITRE DE LA HOMEPAGE">
+                <button>Je suis un bouton</button>
+            </DashboardHeader>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+
+@import '@/assets/sass/dashboard-styles/dashboardPageStyle.scss'
+
+</style>
