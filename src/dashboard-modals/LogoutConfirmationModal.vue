b4db61d8aa5814310f9587b0038ce584d1f1fc31
François-Xavier Savary <fx.savary@orange.fr>
2024-02-23 13:42:11 +0100

    LogoutConfirmationModal ok

--- src/dashboard-modals/LogoutConfirmationModal.vue
+++ src/dashboard-modals/LogoutConfirmationModal.vue
@@ -4,6 +4,43 @@ import { Icon } from '@iconify/vue';
 import ReusableSeparatorDark from '@/sub-components/ReusableSeparatorDark.vue';
 import ReusablePrimaryButton from '@/sub-components/ReusablePrimaryButton.vue';
 import ReusableSecondaryButton from '@/sub-components/ReusableSecondaryButton.vue';
+import { ref, onMounted } from 'vue';
+import { useAdminStore } from '@/stores/AdminStore';
+import { useRouter } from 'vue-router';
+
+// statut par défaut de la visibilité de la fenetre
+const isLogoutConfirmationModalVisible = ref<boolean>(false);
+
+// permet la fermeture de la fenetre au click du bouton Annuler
+const closeLogoutConfirmationModal = (): void => {
+    isLogoutConfirmationModalVisible.value = false;
+};
+
+// ecoute l'événement personnalisé (créé sur 'BackOfficeNavAside') pour réafficher la fenetre
+onMounted(() => {
+    window.addEventListener('show-logoutConfirmationModal', () => {
+        isLogoutConfirmationModalVisible.value = true;
+    });
+});
+
+// gère le bouton de déconnexion et renvoie sur la page de connexion
+const adminStore = useAdminStore();
+const router = useRouter();
+
+const handleSignOut = (): void => {
+
+    // supprime le token du localStorage
+    localStorage.removeItem('token');
+    // supprime les données administrateur du localStorage
+    localStorage.removeItem('adminData');
+    // réinitialise le store
+    adminStore.clearToken();
+    // passe le statut de l'administrateur sur 'déconnecté'
+    adminStore.isConnected = false;
+
+    // redirige vers la page de connexion
+    router.push('/');
+};
 
 </script>
 
@@ -31,10 +68,10 @@ import ReusableSecondaryButton from '@/sub-components/ReusableSecondaryButton.vu
 <style lang="scss" scoped>
 
 @import '@/assets/sass/dashboard-styles/modalStyle';
-/*
-    .hiddenLogoutConfirmationModal {
-        visibility: hidden;
-        opacity: 0;
-    }
-*/
+
+.hiddenLogoutConfirmationModal {
+    visibility: hidden;
+    opacity: 0;
+}
+
 </style>
