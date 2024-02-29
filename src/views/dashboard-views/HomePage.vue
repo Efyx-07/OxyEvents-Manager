db176ead999c7fd9508923b14f4135773f3d2c06
François-Xavier Savary <fx.savary@orange.fr>
2024-02-29 16:32:15 +0100

    no events mention

--- src/views/dashboard-views/HomePage.vue
+++ src/views/dashboard-views/HomePage.vue
@@ -6,6 +6,17 @@ import DashboardEventCard from '@/dashboard-components/DashboardEventCard.vue';
 import RemoveEventConfirmationModal from '@/dashboard-modals/RemoveEventConfirmationModal.vue';
 import DashboardEventsFilters from '@/dashboard-components/DashboardEventsFilters.vue';
 import EventsSearchBar from '@/dashboard-components/EventsSearchBar.vue';
+import ReusablePrimaryButton from '@/sub-components/ReusablePrimaryButton.vue';
+import { Icon } from '@iconify/vue';
+import { useEventStore } from '@/stores/EventStore';
+import { computed } from 'vue';
+
+const eventStore = useEventStore();
+
+// permet de vérifier le nombre d'évènements créés
+const eventsCount = computed<number>(() => {
+    return eventStore.events.length;
+});
 
 </script>
 
@@ -20,6 +31,13 @@ import EventsSearchBar from '@/dashboard-components/EventsSearchBar.vue';
                 <div class="eventCards_container">
                     <DashboardEventCard />
                 </div>
+                <div v-if="eventsCount === 0" class="no-events">
+                    <p>Il n'y a aucun évènement pour l'instant...</p>
+                    <Icon icon="streamline-emojis:magnifying-glass-tilted-left"  class="icon"/>
+                    <router-link to="/event-create">
+                        <ReusablePrimaryButton>Créez votre 1er évènement</ReusablePrimaryButton>
+                    </router-link>
+                </div>
             </div>
             <EventsSearchBar class="eventsSearchBar"/>
         </div>
@@ -30,6 +48,7 @@ import EventsSearchBar from '@/dashboard-components/EventsSearchBar.vue';
 <style lang="scss" scoped>
 
 @import '@/assets/sass/dashboard-styles/dashboardPageStyle.scss';
+@import '@/assets/sass/dashboard-styles/colors.scss';
 
 .eventCards_container {
     display: flex;
@@ -42,6 +61,23 @@ import EventsSearchBar from '@/dashboard-components/EventsSearchBar.vue';
     position: fixed;
     top: 0;
     z-index: 198;
+}
+.no-events {
+    width: 100%;
+    height: 75vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+
+    p {
+        margin: 0;
+        color: $whiteColor; 
+    }
+    .icon {
+        font-size: 6rem;
+    }
 }
 
 </style>
