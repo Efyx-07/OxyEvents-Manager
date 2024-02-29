b5361fee83b760140e2112f2ad23d93ae0f9953d
François-Xavier Savary <fx.savary@orange.fr>
2024-02-29 12:15:54 +0100

    DashboardEventsFilters scss

--- src/dashboard-components/DashboardEventsFilters.vue
+++ src/dashboard-components/DashboardEventsFilters.vue
@@ -2,6 +2,7 @@
 
 import { useEventStore } from '@/stores/EventStore';
 import { computed, ref, onMounted } from 'vue';
+import SearchIcon from '@/sub-components/SearchIcon.vue';
 
 const eventStore = useEventStore();
 
@@ -55,22 +56,92 @@ const resetActiveTabs = () => {
     currentFilter.value = '';
 };
 
+// crée une interface de type pour FilterItem
+interface FilterItem {
+    name: string;
+    clickAction: Function;
+    currentFilter: string;
+    count: number;
+};
+
+// datas des filterItems
+const filterItems: FilterItem[] = [
+
+    {
+        name: 'Tous',
+        clickAction: () => displayAllEvents(),
+        currentFilter: 'all',
+        count: eventsCount.value,
+    },
+
+    {
+        name: 'A venir',
+        clickAction: () => displayOnlyUpcomingEvents(),
+        currentFilter: 'upcoming',
+        count: upcomingEventsCount.value,
+    },
+
+    {
+        name: 'Passés',
+        clickAction: () => displayOnlyPastEvents(),
+        currentFilter: 'past',
+        count: pastEventsCount.value,
+    }
+]
+
 </script>
 
 <template>
-    <div id="eventsNav" class="boHeaderNav" v-if="eventsCount >= 1">
-        <!-- <SearchIcon class="searchIcon" /> -->
-        <div class="navItem" @click="displayAllEvents" :class="{ 'active-link': currentFilter === 'all' }">
-            <p>Tous <span>({{ eventsCount }})</span></p>
-            <div class="activeBar"></div>
+    <div class="eventsFilters-container" v-if="eventsCount >= 1">
+        <div class="searchIcon-container">
+            <SearchIcon />
         </div>
-        <div class="navItem" @click="displayOnlyUpcomingEvents" :class="{ 'active-link': currentFilter === 'upcoming' }">
-            <p>A venir <span>({{ upcomingEventsCount }})</span></p>
-            <div class="activeBar"></div>
-        </div>
-        <div class="navItem" @click="displayOnlyPastEvents" :class="{ 'active-link': currentFilter === 'past' }">
-            <p>Passés <span>({{ pastEventsCount }})</span></p>
-            <div class="activeBar"></div>
+        <div class="filterItems-container">
+            <div class="filterItem" v-for="filterItem in filterItems" :key="filterItem.name" @click="filterItem.clickAction" :class="{ 'active-link': currentFilter === 'filterItem.currentFilter' }">
+                <p>{{ filterItem.name }} <span>({{ filterItem.count }})</span></p>
+                <div class="activeBar"></div>
+            </div>
         </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+
+@import '@/assets/sass/dashboard-styles/colors.scss';
+@import '@/assets/sass/variables.scss';
+@import '@/assets/sass/breakPoints.scss';
+.eventsFilters-container {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    .filterItems-container {
+        width: 100%;
+        border: solid 1px $whiteTransparency;
+        border-radius: $containerRadius * 2;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        padding: .5rem 2rem;
+        align-items: center;
+        .filterItem {
+            p {
+                margin: 0;
+                &:hover, &.active-link {
+                    color: $accentColorPrimary;
+                }
+            }
+        }
+    }
+}
+
+@media screen and (min-width: $breakpointDesktop) {
+    .eventsFilters-container {
+        gap: 2rem;
+
+        .filterItems-container {
+            gap: 5rem;
+        }
+    }
+}
+
+</style>
---
+++ src/sub-components/SearchIcon.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+import { Icon } from '@iconify/vue';
+
+/*
+// ouvre la fenetre 'EventsSearchbar' au clic de l'icone
+const revealEventsSearchBar = () => {
+    // crée un nouvel evenement personnalisé 
+    const showEventsSearchBarEvent = new Event('show-eventsSearchBar');
+    // déclenche l'événement 'show-eventsSearchBar' sur l'objet window
+    window.dispatchEvent(showEventsSearchBarEvent);
+};*/
+
+</script>
+
+<template>
+    <div class="searchIcon_container" @click="revealEventsSearchBar">
+        <Icon icon="ic:sharp-search" class="searchIcon" />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+@import '@/assets/sass/dashboard-styles/colors.scss';
+@import '@/assets/sass/breakPoints.scss';
+.searchIcon_container {
+    width: 2rem;
+    height: 2rem;
+    background: $accentColorSecondary;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    &:hover {
+        background: $accentColorSecondaryLighted;
+    }
+    .searchIcon {
+        color: $whiteColor;
+        font-size: 1.3rem;
+    }
+}
+
+@media screen and (min-width: $breakpointDesktop) {
+    .searchIcon_container {
+        width: 2.3rem;
+        height: 2.3rem;
+    }
+}
+
+</style>
