4ee4b52fd11c0d8209594b65186cf61b169ba8dc
François-Xavier Savary <fx.savary@orange.fr>
2024-03-02 15:23:22 +0100

    EventPage sass setup

--- src/views/public-views/EventPage.vue
+++ src/views/public-views/EventPage.vue
@@ -21,8 +21,79 @@ const selectedEvent: Event | undefined = allEvents.find((event) => {
 </script>
 
 <template>
-    <div>
-        Je suis eventPage
-        <h1 v-if="selectedEvent">{{ selectedEvent.title }}</h1>
+    <div class="eventPage">
+        <EP_Header :selectedEvent="selectedEvent"/>
+        <div class="eventPage_content">
+            <div class="eventCard_container" ref="eventCard">
+                <EP_EventCard :selectedEvent="selectedEvent" />
+            </div>
+            <div class="eventPage-sections_container">
+                <div id="presentation">
+                    <EP_Section_presentation :selectedEvent="selectedEvent"/>
+                </div>
+                <div id="programme">
+                    <EP_Section_programme :selectedEvent="selectedEvent"/>
+                </div>
+                <div id="practicalInformations">
+                    <EP_Section_practicalInformations :selectedEvent="selectedEvent"/>
+                </div>
+            </div>
+        </div>
+        <EP_Footer />
+        <div class="stickyBarContainer">
+            <EP_StickyBar :selectedEvent="selectedEvent"/>
+        </div>
     </div>
+    <EP_Modal_participantsInscription :selectedEvent="selectedEvent"/>   
 </template>
+
+<style lang="scss" scoped>
+
+@import '@/assets/sass/breakPoints.scss';
+@import '@/assets/sass/eventPage-styles/colors.scss';
+
+.eventPage {
+    background: $whiteBackground;
+    color: $blackColor;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    &_content {
+        width: 100%;
+        max-width: 77rem;
+        display: flex;
+        flex-direction: column;
+        .eventPage-sections_container {
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+            padding: 3rem 1rem 6rem 1rem;
+        }
+    }
+    .stickyBarContainer {
+        display: none;
+    }
+}
+@media screen and (min-width: $breakpointDesktop) {
+    .eventPage {
+        padding: 0;
+        position: relative;
+
+        &_content {
+            padding: 0 1rem;
+            .eventPage-sections_container {
+                padding: 3rem 0rem 6rem 0rem;
+            }
+        }
+        .stickyBarContainer {
+            display: block;
+            position: fixed;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+        }
+    }
+}
+       
+</style>
