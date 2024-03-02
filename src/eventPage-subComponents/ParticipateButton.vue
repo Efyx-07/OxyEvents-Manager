da75225b8c3fa86b4d4bc6c99498aa05436220f5
François-Xavier Savary <fx.savary@orange.fr>
2024-03-02 17:09:27 +0100

    buttonStyle sass

--- src/eventPage-subComponents/ParticipateButton.vue
+++ src/eventPage-subComponents/ParticipateButton.vue
@@ -6,30 +6,8 @@
 
 <style lang="scss" scoped>
 
-@import '@/assets/sass/eventPage-styles/colors.scss';
+@import '@/assets/sass/eventPage-styles/buttonStyle.scss';
 @import '@/assets/sass/breakPoints.scss';
-@import '@/assets/sass/variables.scss';
-
-.btn {
-    width: 100%;
-    height: 3rem;
-    background: $accentColorSecondary;
-    border: none;
-    border-radius: $containerRadius * 2;
-    box-shadow: $shadow;
-    cursor: pointer;
-
-    &:hover {
-        background: $accentColorPrimary;
-    }
-
-    p {
-        margin: 0;
-        font-size: 1.25rem;
-        font-weight: 600;
-        color: $whiteColor;
-    }
-}
 
 @media screen and (min-width: $breakpointDesktop) {
 
