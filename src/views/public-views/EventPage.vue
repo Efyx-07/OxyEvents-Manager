530a211f9b2a122e28ccc8fa06d167bd20bc479c
François-Xavier Savary <fx.savary@orange.fr>
2024-03-02 15:07:32 +0100

    EventPage setup

--- src/views/public-views/EventPage.vue
+++ src/views/public-views/EventPage.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import { useEventStore } from '@/stores/EventStore';
+import { useRoute } from 'vue-router';
+import type { Event } from '@/types/eventsTypes';
+
+const eventStore = useEventStore();
+const route = useRoute();
+
+const allEvents: Event[] = eventStore.events;
+
+// récupère le slug de la route
+const eventSlug: string | string[] = route.params.eventSlug; 
+
+// compare pour faire correspondre les slugs
+const selectedEvent: Event | undefined = allEvents.find((event) => {
+    return event.slug === eventSlug;
+});
+
+
+</script>
 
 <template>
     <div>
         Je suis eventPage
+        <h1 v-if="selectedEvent">{{ selectedEvent.title }}</h1>
     </div>
 </template>
