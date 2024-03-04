9c9505e17ef777ee6783d2669804eb845e0a1425
François-Xavier Savary <fx.savary@orange.fr>
2024-03-04 18:45:06 +0100

    Footer

--- src/eventPage-components/EventPageFooter.vue
+++ src/eventPage-components/EventPageFooter.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
 
-import ParticipateButton from '@/eventPage-subComponents/ParticipateButton.vue';
-import { Icon } from '@iconify/vue';
-
-// permet le retour vers le haut de page
-const scrollToTop = () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
-};
+//import ParticipateButton from '@/eventPage-subComponents/ParticipateButton.vue';
+import ToTopButton from '@/eventPage-subComponents/ToTopButton.vue';
 
 </script>
 
@@ -20,15 +12,48 @@ const scrollToTop = () => {
                 <p>Conditions d'utilisation</p>
                 <p>Contact</p>
             </div>
-            <div class="toTopButton_container" @click="scrollToTop">
-                <Icon icon="iconamoon:arrow-up-2-duotone" class="toTopIcon"/>
-            </div>
+            <ToTopButton />
         </div>
     </div>
-    <div class="footer_mobile">
+    <!-- <div class="footer_mobile">
         <ParticipateButton class="footer_mobile-button"/>
         <div class="mobileToTopButton_container" @click="scrollToTop">
             <Icon icon="iconamoon:arrow-up-2-duotone" class="mobileToTopIcon"/>
         </div>
-    </div>    
+    </div>     -->
 </template>
+
+<style lang="scss" scoped>
+
+@import '@/assets/sass/eventPage-styles/colors.scss';
+
+.footer {
+    width: 100%;
+    height: 8rem;
+    background: $whiteColor;
+    border-radius: 50% 50% 0 0;
+    display: flex;
+    justify-content: center;
+
+    .content {
+        width: 100%;
+        max-width: 77rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 1rem;
+
+        .links_container {
+            display: flex;
+            gap: 3rem;
+
+            p {
+                margin: 0;
+                font-size: .9rem;
+                font-weight: 500;
+            }
+        }
+    }
+}
+
+</style>
---
+++ src/eventPage-subComponents/ToTopButton.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+import { Icon } from '@iconify/vue';
+
+// permet le retour vers le haut de page
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
+
+</script>
+
+<template>
+    <div class="toTopButton_container" @click="scrollToTop">
+        <Icon icon="iconamoon:arrow-up-2-duotone" class="toTopIcon"/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+@import '@/assets/sass/eventPage-styles/colors.scss';
+
+.toTopButton_container {
+    width: 3rem;
+    height: 3rem;
+    background: $accentColorPrimary;
+    box-shadow: $shadow;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    &:hover {
+        background: $accentColorSecondary;
+    }
+
+    .toTopIcon {
+        color: $whiteColor;
+        font-size: 2.5rem;
+    }
+}
+
+</style>
